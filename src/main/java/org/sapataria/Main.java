0b9f20414e2989207da23ca0ebcb9be79acf032d
lamarcke <cassiolamarcksilvafreitas@gmail.com>
2022-08-20 00:16:24 -0300

    Initial commit

---
+++ src/main/java/org/sapataria/Main.java
@@ -0,0 +1,10 @@
+package org.sapataria;
+
+import org.sapataria.helpers.CPF;
+
+public class Main {
+    public static void main(String[] args) {
+        CPF cpf = new CPF("62412397325");
+    }
+    
+}
